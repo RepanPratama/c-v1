930d0e3fc52098613d92a689a9ff97c0309c360e
RepanPratama <50663231+RepanPratama@users.noreply.github.com>
2022-09-29 15:29:20 +0700

    update files
    tester

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char** argv) {
+	int a = 1;
+	int b = 2;
+	int c = a + b;
+	int d = a / b;
+	int e = a - b;
+	int f = a * b;
+	/* hasil */
+	printf("isi variabel %d \n",c);
+	printf("isi variabel %d \n",d);
+	printf("isi variabel %d \n",e);
+	printf("isi variabel %d \n",f);
+	return 0;
+}
